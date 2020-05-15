e61ef21451fde00602ef4498c5275adca454a0a3
びくんびくん <big.stone.net@gmail.com>
2020-05-15 20:03:34 +0900

    add layout and layout change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+  private def set_layout
+    if params[:controller].match(%r{\A(admin)})
+      Regrep.last_match[1]
+    else
+      "application"
+    end
+  end
 end
