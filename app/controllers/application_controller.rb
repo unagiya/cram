cce85793cea87cf44b205802e01b52dc50179dd4
びくんびくん <big.stone.net@gmail.com>
2020-07-17 17:41:02 +0900

    mod application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   layout :set_layout
 
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+
   private def set_layout
     if params[:controller].match(%r{\A(admin)/})
       Regexp.last_match[1]
@@ -14,3 +17,4 @@ class ApplicationController < ActionController::Base
     render "errors/internal_server_error", status: 500
   end
 end
+
